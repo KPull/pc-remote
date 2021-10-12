10783bef87429f1848ddbd4418cd6443b8cc9a64
Kyle Pullicino <kyle.pullicino@phalanx.software>
2021-10-12 21:11:12 +0200

    Add Twitch and shutdown endpoints

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-if __name__ == '__main__':
-    pass
+import asyncio
+
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+
+def create_server():
+    application = FastAPI()
+
+    @application.get("/twitch")
+    async def twitch(channel: str):
+        await asyncio.create_subprocess_shell(F"streamlink -p mpv https://www.twitch.tv/{channel} best")
+        return {"status": "OK"}
+
+    @application.get("/shutdown")
+    async def shutdown():
+        await asyncio.create_subprocess_shell(F"shutdown now")
+        return {"status": "OK"}
+
+    return application
+
+
+server = create_server()
